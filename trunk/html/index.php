a6c17e0aba1f3607bf4d43b71a447e13939d3d7a
mlittle <mlittle@b8a3114e-d356-49c1-bee0-d68937b74f6f>
2009-09-13 00:32:42 +0000

    merging MAL Milk changes with Milk repo
    git-svn-id: https://seed1.projectlocker.com/sau/Milk/svn@19 b8a3114e-d356-49c1-bee0-d68937b74f6f

--- trunk/html/index.php
+++ trunk/html/index.php
@@ -1,7 +1,15 @@
 <?php
 
+    /**
+     * This is the bare minimum that is required in this file to get Milk up and running.
+     * You may also want to load additional files/libraries or implement your URL
+     * rewriting etc here (for example to map /myfile.html to the myfile module)
+     */
 
-    include($_SERVER['DOCUMENT_ROOT'] . '/milk/milk.php');
+    define('MILK_PATH', dirname($_SERVER['SCRIPT_FILENAME']));
+    include(MILK_PATH . '/milk/milk.php');
+
+    MilkLauncher::loadConfig();
 
     $milk = new MilkLauncher('Home');
     $milk->module->run();
